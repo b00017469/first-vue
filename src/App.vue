c11b0dca5e1fa3ccfc59d79fb2ccfc572e5c5f3a
Pavel Morozuk <morozuki@mail.ru>
2023-01-19 23:41:48 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script xmlns="http://www.w3.org/1999/html">
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    addCount() {
+      this.count += 1;
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    Count - {{ count }}
+    <button @click="addCount">Add count</button>
+  </div>
+</template>
+
+<style>
+
+</style>
