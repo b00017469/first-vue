47d6988f87ac48f8e7c7e35e810dca2f87cc0e25
Pavel Morozuk <morozuki@mail.ru>
2023-01-25 14:57:54 +0300

    posts animation

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,11 +1,13 @@
 <template>
   <div v-if="posts.length > 0">
     <h4>Список постов:</h4>
-    <PostItem :post="post"
-              :key="post.id"
-              v-for="post in posts"
-              @remove="$emit('remove', post)"
-    />
+    <transition-group name="post-list">
+      <PostItem :post="post"
+                :key="post.id"
+                v-for="post in posts"
+                @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <h3 v-else style="color: red">
     Список постов пуст
@@ -27,5 +29,20 @@ export default {
 </script>
 
 <style scoped>
-
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.6s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to{
+  opacity: 0;
+  transform: translateY(30px);
+}
+.post-list-move {
+  transition: transform 0.6s ease;
+}
 </style>
